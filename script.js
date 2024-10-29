1778a55848e805e5eb87bcdab169d74efe390bf9
SHERLOCK363 <shlomokuruvillamathew2@gmail.com>
2024-10-29 04:14:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validatePasswords() {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return false; // Prevent form submission
+    }
+    return true; // Allow form submission
+}
